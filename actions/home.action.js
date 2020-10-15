d36b012f5aff010a08f11b7a98908a697e98a3f3
Yuan Yao <chris@Yuans-MacBook-Pro.local>
2020-10-14 17:26:06 -0700

    SAVE FOR PREAPARE MERGE

--- actions/home.action.js
+++ actions/home.action.js
@@ -1,17 +1,10 @@
 class HomeAction {
-
-
     static findContactInfo() {
-        const contact_info = require('../data/contact');
-        return contact_info || {
-            "name": "Bai Chi",
-            "email": "baichi950217@gmail.com",
-            "location": "Chino Hills, CA",
-            "phone": "+1 626 417 8309",
-            "address": "55223 Avata Blvd",
-            "job_title": "House Wife",
-            "age": "99"
-        }
+        return require('../data/contact');
+    }
+
+    static findAboutMeInfo(){
+        return require('../data/about');
     }
 }
 
